25e5b80a6e1cd1cadd0431d6ee9323412160f2cc
theFellandes <57584110+theFellandes@users.noreply.github.com>
2021-10-22 21:48:40 +0300

    Strategy design started

---
+++ BTK-Strategy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BTK_Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using before2010");
+        }
+    }
+
+    class After2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using after2010");
+        }
+    }
+
+    class CustomerManager
+    {
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer manager business");
+            //Kredisini hesaplama kısmı, eğer üstteki kodları yazmasaydık if şöyleyse if böyleyse demek gerekecekti.
+        }
+    }
+}
