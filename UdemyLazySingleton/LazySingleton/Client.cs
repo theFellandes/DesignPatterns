cdc2aa9f4e24eb6947cc69917823d49f2a02ccda
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-03-06 15:24:37 +0300

    Lazy singleton added

---
+++ UdemyLazySingleton/LazySingleton/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UdemyLazySingleton.LazySingleton
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+               LazySingleton s = LazySingleton.Instance;
+                s.PrintName();
+            }
+        }
+    }
+}
---
+++ UdemyLazySingleton/LazySingleton/LazySingleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UdemyLazySingleton.LazySingleton
+{
+    public class LazySingleton
+    {
+        private static LazySingleton _singleton;
+
+        private static int _count;
+        private string _name;
+
+        private LazySingleton()
+        {
+            _count++;
+            _name = "LazySingleton" + _count;
+        }
+
+        public static LazySingleton Instance => _singleton ??= new LazySingleton();
+
+        public void PrintName()
+        {
+            Console.WriteLine(_name);
+        }
+    }
+}
