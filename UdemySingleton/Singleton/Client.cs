63813e09c55218a7cc4db0d84ac542715b7efd94
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-03-06 15:07:29 +0300

    Singleton added

---
+++ UdemySingleton/Singleton/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UdemySingleton.Singleton
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Singleton s = Singleton.Instance;
+                s.PrintName();
+            }
+            
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+
+            Console.WriteLine(s1 == s2 ? "\nThe same object!" : "Different objects!");
+        }
+    }
+}
---
+++ UdemySingleton/Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UdemySingleton.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _singleton = new Singleton();
+        private static int _count;
+        private readonly string _name;
+
+        private Singleton()
+        {
+            _count++;
+            _name = "Singleton " + _count;
+        }
+
+        public static Singleton Instance => _singleton;
+
+        public void PrintName()
+        {
+            Console.WriteLine(_name);
+        }
+    }
+}
